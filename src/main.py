067cf797b0df7c1d175e0fe7daadd1f12aef0977
Maering <salomon.segan@gmail.com>
2019-03-12 15:43:43 +0100

    Updated methods
    Added pictures

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import cv2
-import numpy
+import numpy as np
+from matplotlib import pyplot as plt
 
 ###############################
 # Script to detect road signs #
@@ -15,35 +16,50 @@ import numpy
 #     - : https://www.researchgate.net/publication/281368553_Traffic_sign_recognition_without_color_information
 ###############################
 
+def show(img):
+    pltImage = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    plt.imshow(pltImage)
+    plt.show()
+
 # FIXME
 def analyse(path):
-    img = cv2.imread(path)
+    img = cv2.imread(path, 0)
+    img = cv2.medianBlur(img, 5)
     assert img is not None and img.size != 0, 'Invalid image'
-    cv2.imshow('reference image',img)
-
-    # Gray scale
-    imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    cv2.imshow('reference image',imgGray)
-
+    
     # Normalize
-    imgNormalized = cv2.normalize(imgGray,None,0,255,cv2.NORM_MINMAX)
-    cv2.imshow('imgNormalized',imgNormalized)
-
-    # Edge detection
-    imgEdges = cv2.Canny(imgNormalized,img.width,img.height)
+    imgNormalized = cv2.normalize(img,None,0,255,cv2.NORM_MINMAX)
 
     # Math morph
-    kernel = np.ones((5,5),np.uint8)
-    imgOpening = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
+    kernel = np.ones((3,3),np.uint8)
 
-    # Edge 2.0
-    imgGradient = cv2.morphologyEx(img, cv2.MORPH_GRADIENT, kernel)
+    # Dilate normalized image, try also with erode
+    dilate = cv2.dilate(imgNormalized,kernel,iterations = 1)
+
+    # Ouverture
+    imgOpening = cv2.morphologyEx(dilate, cv2.MORPH_OPEN, kernel)
+    
+    show(imgOpening)
+
+    # Canny (Edge)
+    imgEdges = cv2.Canny(imgOpening, 150 , 220)
+
+    show(imgEdges)
+
+    # Edge Fermeture
+    imgGradient = cv2.morphologyEx(imgEdges, cv2.MORPH_GRADIENT, kernel)
+
+    # Affichage
+    show(imgGradient)
 
     # TODO: segmentation
 
 
 # ___ MAIN ___
-analyse('..\\images\\test.png')
+analyse('C:\\Dev\\!Traitement_Images\\Panno\\images\\test.png')
+analyse('C:\\Dev\\!Traitement_Images\\Panno\\images\\many.jpg')
+analyse('C:\\Dev\\!Traitement_Images\\Panno\\images\\autobahn.png')
+analyse('C:\\Dev\\!Traitement_Images\\Panno\\images\\yellow_roadsign.jpg')
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
