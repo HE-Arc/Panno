8c5687e11282f9782b83060962d9fd0a1e892ab3
Maering <salomon.segan@gmail.com>
2019-04-02 13:51:20 +0200

    Crop circle

--- src/main.py
+++ src/main.py
@@ -20,6 +20,9 @@ from matplotlib import pyplot as plt
 # TODO: Appliquer les algos sur des zones de couleurs vives
 # FIXME: Améliorer avec des analyses par couleur, exemple : Hough sur le bleu
 
+# RGB, HSV, 
+# Multi-résolution, si on trouve un élément à la même place pour chaque résolution on est sur qu'il existe
+
 def hough_lines(img_src, img_dest):
     lines = cv2.HoughLines(img_src,1,np.pi/180.0, 150)
     ''' 
@@ -27,19 +30,19 @@ def hough_lines(img_src, img_dest):
     5th parameter : Minimum length of line
     6th parameter : Maximum allowed gap between line segments to treat them as single line
     '''
- 
-    for rho,theta in lines[0]:
-        a = np.cos(theta)
-        b = np.sin(theta)
-        x0 = a*rho
-        y0 = b*rho
-        x1 = int(x0 + 1000*(-b))
-        y1 = int(y0 + 1000*(a))
-        x2 = int(x0 - 1000*(-b))
-        y2 = int(y0 - 1000*(a))
-        cv2.line(img_dest,(x1,y1),(x2,y2),(255,0,255),5)
+    if lines:
+        for rho,theta in lines[0]:
+            a = np.cos(theta)
+            b = np.sin(theta)
+            x0 = a*rho
+            y0 = b*rho
+            x1 = int(x0 + 1000*(-b))
+            y1 = int(y0 + 1000*(a))
+            x2 = int(x0 - 1000*(-b))
+            y2 = int(y0 - 1000*(a))
+            cv2.line(img_dest,(x1,y1),(x2,y2),(255,0,255),5)
 
-    return img_dest
+        return img_dest
 
 def hough_lines_p(img_src, img_dest, minimum_length, max_gap):
     lines = cv2.HoughLinesP(img_src,1,np.pi/180.0, 150, minimum_length, max_gap)
@@ -58,15 +61,32 @@ def hough_lines_p(img_src, img_dest, minimum_length, max_gap):
     
     return img_dest
 
+def hough_circles(img_src, img_dest):
+    # FIXME: Trouver des bon ratio
+    circles = cv2.HoughCircles(img_src, cv2.HOUGH_GRADIENT, 1.1, 35)
+
+    # convert the (x, y) coordinates and radius of the circles to integers
+    circles = np.round(circles[0, :]).astype("int")
+
+    for (x, y, r) in circles:
+        # draw the outer circle
+        cv2.circle(img_dest, (x, y), r, (0, 255, 0), 4)
+
+        # Crop circle
+        crop_img = img_dest[y-r:y+r, x-r:x+r]
+        plt.imshow(crop_img)
+        plt.show()
+
+
+    return img_dest
+
 def show(img):
     pltImage = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     plt.imshow(pltImage)
     plt.show()
 
 # FIXME
-def analyse(path):
-    img = cv2.imread(path)
-    assert img is not None and img.size != 0, 'Invalid image'
+def analyse(img):
 
     height, width, channels = img.shape
     print(width)
@@ -100,25 +120,20 @@ def analyse(path):
     # Show edges
     show(imgGradient)
 
-    # Probabilistic Hough
-    minimum_length = width/100.0
-    max_gap = minimum_length - minimum_length * 0.25
+    try:
+        # Probabilistic Hough
+        minimum_length = width/100.0
+        max_gap = minimum_length - minimum_length * 0.25
+        img = hough_lines_p(imgGradient, img, minimum_length, max_gap)
+    except:
+        print('can\'t perform hough (lines)')
 
-    img = hough_lines_p(imgGradient, img, minimum_length, max_gap)
-
-    # Hough Circles
-    # FIXME: Trouver des bon ratio
-    circles = cv2.HoughCircles(imgGradient, cv2.HOUGH_GRADIENT, 1.1, 35)
-
-    # ensure at least some circles were found
-    if circles is not None:
-        # convert the (x, y) coordinates and radius of the circles to integers
-        circles = np.round(circles[0, :]).astype("int")
+    try:
+        # Hough Circles
+        img = hough_circles(imgGradient, img)
+    except:
+        print('can\'t perform hough (circles)')
     
-        for (x, y, r) in circles:
-            # draw the outer circle
-            cv2.circle(img, (x, y), r, (0, 255, 0), 4)
-
 
     # Affichage
     show(img)
@@ -127,10 +142,19 @@ def analyse(path):
 
 
 # ___ MAIN ___
-analyse('C:\\Dev\\!Traitement_Images\\Panno\\images\\test.png')
-analyse('C:\\Dev\\!Traitement_Images\\Panno\\images\\many.jpg')
-analyse('C:\\Dev\\!Traitement_Images\\Panno\\images\\autobahn.png')
-analyse('C:\\Dev\\!Traitement_Images\\Panno\\images\\yellow_roadsign.jpg')
+
+img = cv2.imread('C:\\Dev\\!Traitement_Images\\Panno\\images\\test.png')
+assert img is not None and img.size != 0, 'Invalid image'
+half = cv2.resize(img, (0,0), fx=0.5, fy=0.5) 
+quarter = cv2.resize(img, (0,0), fx=0.25, fy=0.25) 
+
+analyse(img)
+analyse(half)
+analyse(quarter)
+
+#analyse('C:\\Dev\\!Traitement_Images\\Panno\\images\\many.jpg')
+#analyse('C:\\Dev\\!Traitement_Images\\Panno\\images\\autobahn.png')
+#analyse('C:\\Dev\\!Traitement_Images\\Panno\\images\\yellow_roadsign.jpg')
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
